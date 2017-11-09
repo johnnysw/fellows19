f915221ed8d0cd5b1948af5f72d26ff343397f7d
xiecheng <13136666328@163.com>
2017-11-09 20:30:32 +0800

    复习

--- DOM/jslib.js
+++ DOM/jslib.js
@@ -30,6 +30,15 @@ function getByClassName(clsName, context) {
     }
 }
 
-function test(){
-    alert(123);
+//实现深拷贝
+function clone(obj){
+    var newObj = {};
+    for(var p in obj){
+        if(typeof obj[p] == 'object'){
+            newObj[p] = clone(obj[p]);
+        }else{
+            newObj[p] = obj[p];
+        }
+    }
+    return newObj;
 }
