948872011c43dc40acd126b115a29696b602c76e
xiecheng <13136666328@163.com>
2017-12-03 11:54:52 +0800

    requirejs

--- requirejs/index2.js
+++ requirejs/index2.js
@@ -1,16 +1,16 @@
 //requirejs  AMD
-// require(['sort'], function (sortArr) {
-//     var arr = [5, 3, 11, 2, 1];
-//     // var arr = '12313';
-//     //...
-//     console.log(sortArr(arr));
-// });
+require(['sort'], function (sortArr) {
+    var arr = [5, 3, 11, 2, 1];
+    // var arr = '12313';
+    //...
+    console.log(sortArr(arr));
+});
 
 
 //seajs  CMD
-define(function(require){
+/* define(function(require){
     var arr = [5, 3, 11, 2, 1];
     //...
     var sortArr = require('sort');
     console.log(sortArr(arr));
-});
+}); */
